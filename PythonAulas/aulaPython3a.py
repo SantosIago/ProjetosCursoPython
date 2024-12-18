7d1045a4e905b6369a257d212516f466321f81a7
Iago456 <iagosantossud2019@gmail.com>
2024-12-18 18:33:58 -0300

    Mais dos meus projetos do curso de Python

---
+++ PythonAulas/aulaPython3a.py
@@ -0,0 +1,6 @@
+print ('===== DESAFIO 03 =====')
+primeiro = int(input ('Digite o primeiro número: '))
+segundo = int(input ('Digite o segundo número: '))
+resultado = primeiro + segundo
+#  print ('A soma entre ', primeiro, 'e', segundo, 'é:', resultado)
+print('A soma entre {} e {} vale {}'.format(primeiro, segundo, resultado))
