3b1929b0c03aa96903f90ea59f55a6c184ef7e88
Iago da Silva Santos <124318273+Iago456@users.noreply.github.com>
2025-01-02 17:10:14 -0300

    Exercício 28 - concluído Escreva um programa que faça o computador "Pensar" em um número inteiro de 0 a 5 e peça para que o usuário tentar descobrir qual foi o número escolhido pelo computador.

--- PythonExercicios/ex028.py
+++ PythonExercicios/ex028.py
@@ -1,12 +1,14 @@
 from random import randint
+from time import sleep
 
-ale = randint(0,5)
-num = int(input('Pensei em um número de 0 a 5. Diga, que número pensei: '))
-if num == ale:
-    print('---------------------------------')
-    print('Parabéns! Você venceu! :)')
-    print('---------------------------------')
-else:
-    print('---------------------------------')
-    print('Que mancada! Você errou, meu número foi {}!'.format(ale))
-    print('---------------------------------')
+ale = randint(0,5)# Faz o computador "PENSAR"
+print("-=-" * 20)
+print("Vou pensar em um número, entre 0 a 5. Tente Adivinhar...")
+print("-=-" * 20)
+num = int(input("Em qual número eu pensei? ")) # O jogador deverá adivinhar
+print('PROCESSANDO...')
+sleep(3)
+if num == ale: # Se o jogador acertar o número pensado
+    print('PARABÉNS! VOCÊ CONSEGUIU ACERTAR! :)')
+else: # Se caso o jogador não acertar o número pensado
+    print('QUE PENA! VOCÊ ERROU, NÃO PENSEI EM {}, PENSEI EM {}'.format(num, ale))
