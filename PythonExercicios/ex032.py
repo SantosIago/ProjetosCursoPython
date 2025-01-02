8eac905d0addf3c0cd0ac635e38bea3b0033c378
Iago da Silva Santos <124318273+Iago456@users.noreply.github.com>
2025-01-02 18:18:01 -0300

    Exercicio 32 - Concluído Faça um programa que leia um ano qualquer e mostre se ele é bissexto. Obs: ao digitar 0, ele verá se o ano atual é bissexto

--- PythonExercicios/ex032.py
+++ PythonExercicios/ex032.py
@@ -1,6 +1,8 @@
-ano = int(input('Por favor! Digite o ano desejado: '))
-bis = ano % 4
-if bis == 0:
-    print('Esse ano é bissexto!')
+from datetime import date
+ano = int(input('Digite o ano desejado! Coloque 0 para analisar o ano atual: '))
+if ano == 0:
+    ano = date.today().year
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print('{} É UM ANO BISSEXTO!'.format(ano))
 else:
-    print('Esse ano não é bissexto!')
+    print('{} NÃO É UM ANO BISSEXTO!'.format(ano))
