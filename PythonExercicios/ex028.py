f07c97acd4d278ce455f33ecb6293ce28a281d89
Iago da Silva Santos <124318273+Iago456@users.noreply.github.com>
2025-01-14 18:17:27 -0300

    Exercicio 28 - concluído
    Escreva um programa que faça o computador "Pesar" em um número inteiro entre 0 e 5 e peça para o usuário tntar descobrir qual foi o número escolhido pelo computador
    O programa deverá escrever na tela se o usuário ganhou ou perdeu.

--- PythonExercicios/ex028.py
+++ PythonExercicios/ex028.py
@@ -6,9 +6,9 @@ print("-=-" * 20)
 print("Vou pensar em um número, entre 0 a 5. Tente Adivinhar...")
 print("-=-" * 20)
 num = int(input("Em qual número eu pensei? ")) # O jogador deverá adivinhar
-print('PROCESSANDO...')
+print('\033[34mPROCESSANDO...\033[m')
 sleep(3)
 if num == ale: # Se o jogador acertar o número pensado
-    print('PARABÉNS! VOCÊ CONSEGUIU ACERTAR! :)')
+    print('\033[32mPARABÉNS! VOCÊ CONSEGUIU ACERTAR! :)')
 else: # Se caso o jogador não acertar o número pensado
-    print('QUE PENA! VOCÊ ERROU, NÃO PENSEI EM {}, PENSEI EM {}'.format(num, ale))
+    print('\033[31mQUE PENA! VOCÊ ERROU, NÃO PENSEI EM {}, PENSEI EM {}'.format(num, ale))
