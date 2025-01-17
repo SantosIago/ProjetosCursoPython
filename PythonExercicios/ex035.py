b31bd909c718b2907b2770ab9bb534284c90d060
Iago da Silva Santos <124318273+Iago456@users.noreply.github.com>
2025-01-16 23:35:01 -0300

    Exercicio 35 - Concluido Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.

--- PythonExercicios/ex035.py
+++ PythonExercicios/ex035.py
@@ -1,13 +1,13 @@
 from time import sleep
-print('-=-' * 20)
+print('\033[35m-=-' * 20)
 print('ANALISADOR DE TRIÂNGULOS')
-print('-=-' * 20)
-l1 = float(input('Digite o valor do primeiro lado: '))
+print('-=-' * 20, '\033[m')
+l1 = float(input('\033[34mDigite o valor do primeiro lado: '))
 l2 = float(input('Digite o valor do segundo lado: '))
 l3 = float(input('Digite o valor do terceiro lado: '))
-print('PROCESSANDO...')
+print('\033[31mPROCESSANDO...')
 sleep(3)
 if l1 < l2 + l3 and l2 < l1 + l3 and l3 < l1 + l2:
-    print('Com os segmentos acima PODEM SE FORMAR um triângulo!')
+    print('\033[34mCom os segmentos acima {}PODEM SE FORMAR{}\033[34m um triângulo!'.format('\033[1;32m', '\033[m'))
 else:
-    print('Com os segmentos acima NÃO PODEM SE FORMAR um triângulo!')
+    print('\033[34mCom os segmentos acima {}NÃO PODEM SE FORMAR{}\033[34m um triângulo!'.format('\033[1;31m', '\033[m'))
