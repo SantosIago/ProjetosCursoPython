ac1a3562fd216df09083eaeea43d8de49d6c7c6c
Iago da Silva Santos <124318273+Iago456@users.noreply.github.com>
2025-01-16 23:59:08 -0300

    Exercicio 19 - Concluido Um profesor quer sortear um dos seus alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.

--- PythonExercicios/ex019.py
+++ PythonExercicios/ex019.py
@@ -1,6 +1,7 @@
 import random
-a1 = str(input('Qual o nome do 1° aluno: '))
+a1 = str(input('\033[1;34mQual o nome do 1° aluno: '))
 a2 = str(input('Qual o nome do 2° aluno: '))
 a3 = str(input('Qual o nome do 3° aluno: '))
 a4 = str(input('Qual o nome do 4° aluno: '))
-print('Por favor, aluno {}, apague o quadro'.format(random.choice([a1, a2, a3, a4])))
+print('Por favor, aluno {}{}{}\033[1;34m, '
+      'apague o quadro'.format('\033[4;32m',random.choice([a1, a2, a3, a4]), '\033[m'))
