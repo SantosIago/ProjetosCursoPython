5e5acbb2e18a767397e4dde46328805f1f52ff85
Iago da Silva Santos <124318273+Iago456@users.noreply.github.com>
2025-01-16 23:45:41 -0300

    Exercicio 31 - cocluido Desenvolva um programa que pergunte a distância de uma viagem em ka. Calcule o preço da passagem, cobrando R$0,50 por km para viajens de até 200km e R$0,45 para viagens mais longas.

--- PythonExercicios/ex031.py
+++ PythonExercicios/ex031.py
@@ -1,9 +1,9 @@
-num = float(input('Digite quantos quilômetros foi a viagem: '))
+num = float(input('\033[4;34mDigite quantos quilômetros foi a viagem: '))
 if num <= 200:
     cobr = num * 0.50
-    print('Ao viajar por menos de 200 km, será cobrado R$0,50 por km\n'
-          'Com isso, o valor a ser pago será de R${}'.format(cobr))
+    print('\033[1;37mAo viajar por menos de 200 km, será cobrado R$0,50 por km\n'
+          'Com isso, o valor a ser pago será de {}R${}'.format('\033[1;32m', cobr))
 else:
     cobr = (num * 0.45)
-    print('Ao viajar por mais de 200 km, será cobrado R$0,45 por km\n'
-          'Com isso, o valor a ser pago pela viagem será de R${}'.format(cobr))
+    print('\033[1;37mAo viajar por mais de 200 km, será cobrado R$0,45 por km\n'
+          'Com isso, o valor a ser pago pela viagem será de {}R${}'.format('\033[1;32m', cobr))
