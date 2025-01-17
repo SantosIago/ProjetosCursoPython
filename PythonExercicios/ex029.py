c72607ceb520f95db4ea0ee0cb0c473e6fc915e5
Iago da Silva Santos <124318273+Iago456@users.noreply.github.com>
2025-01-16 23:16:30 -0300

    Exercicio 29 - Concluido Escreva um programa que leia a velocidade de um carro Se ele ultrapassar 80km/h, mostre uma mensagem qye ele foi multado A multa vai custar R$7,00 por cada km acima do limite.

--- PythonExercicios/ex029.py
+++ PythonExercicios/ex029.py
@@ -1,8 +1,8 @@
-km = int(input('Qual a velocidade atual do carro? '))
+km = int(input('\033[1;33mQUAL A VELOCIDADE ATUAL DO CARRO? '))
 if km > 80:
     mult = (km - 80)
-    print('Você ultrapassou do limite.\n'
-          'sua multa será de: R${:.2f}'.format(km, mult * 7,00))
-    print('Obrigado! Dirija com segurança!')
+    print('\033[1;31mVocê ultrapassou do limite.\n'
+          'sua multa será de: {}R${:.2f}{}'.format( '\033[32m', mult * 7.00, '\033[m'))
+    print('\033[1;36mOBRIGADO! DIRIJA COM SEGURANÇA!')
 else:
-    print('Tudo tranquilo! Você não passou do limite, parabéns!')
+    print('\033[32mTudo tranquilo! Você não passou do limite, parabéns!')
